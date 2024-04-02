34c39208c5554c19b1f92160b3c91172d6069e84
TDMWalpala <tharindudarshana2019@gmail.com>
2024-04-02 10:31:00 +0530

    TA-7:Completed Custom Shape & Clip Path Design

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:t_store/features/shop/home.dart';
+import 'package:t_store/features/shop/screens/home/home.dart';
 import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
 
