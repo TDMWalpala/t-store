739e88b0f6b18c7edfef9401567131cac268e300
TDMWalpala <tharindudarshana2019@gmail.com>
2024-04-06 08:40:31 +0530

    TA-10:Completed CarouselSlider

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,24 +1,26 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:t_store/common/widgets/images/rounded_image.dart';
 import 'package:t_store/common/widgets/text/section_heading.dart';
 import 'package:t_store/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:t_store/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
-
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TPrimaryHeaderContainer(
+            const TPrimaryHeaderContainer(
               child: Column(
                 children: [
                   THomeAppBar(),
@@ -41,6 +43,19 @@ class HomeScreen extends StatelessWidget {
                   )
                 ],
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(TSizes.defaultSpace),
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  viewportFraction: 1
+                ),
+                items: const [
+                  TRoundedImage(imageUrl: TImages.promoBanner1),
+                  TRoundedImage(imageUrl: TImages.promoBanner2),
+                  TRoundedImage(imageUrl: TImages.promoBanner3),
+                ], 
+              ),
             )
           ],
         ),
@@ -48,4 +63,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
