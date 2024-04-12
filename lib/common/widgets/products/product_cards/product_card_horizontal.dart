e8672fa94f3e953e231ddb575178e436be2ba171
TDMWalpala <tharindudarshana2019@gmail.com>
2024-04-12 22:28:28 +0530

    TA-24:Completed Sub Categories

--- lib/common/widgets/products/product_cards/product_card_horizontal.dart
+++ lib/common/widgets/products/product_cards/product_card_horizontal.dart
@@ -1,10 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:t_store/common/widgets/icons/circular_icon.dart';
+import 'package:t_store/common/widgets/images/rounded_image.dart';
+import 'package:t_store/common/widgets/text/brand_title_text_with_verified.dart';
+import 'package:t_store/common/widgets/text/product_price_text.dart';
+import 'package:t_store/common/widgets/text/product_title_text.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
 
 class TProductCardHorizontal extends StatelessWidget {
   const TProductCardHorizontal({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final dark = THelperFunctions.isDarkMode(context);
+    return Container(
+      width: 310,
+      padding: const EdgeInsets.all(1),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(TSizes.productImageRadius),
+        color: dark ? TColors.darkerGrey : TColors.softGrey,
+      ),
+      child: Row(
+        children: [
+          TCircularContainer(
+            height: 120,
+            padding: const EdgeInsets.all(TSizes.sm),
+            background: dark ? TColors.dark : TColors.white,
+            child: Stack(
+              children: [
+                const SizedBox(
+                    width: 120,
+                    height: 120,
+                    child: TRoundedImage(
+                      imageUrl: TImages.productImage1,
+                      applyImageRadius: true,
+                    )),
+                Positioned(
+                  top: 12,
+                  child: TCircularContainer(
+                    radius: TSizes.sm,
+                    background: TColors.secondary.withOpacity(0.8),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: TSizes.sm, vertical: TSizes.xs),
+                    child: Text(
+                      '25%',
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelLarge!
+                          .apply(color: TColors.black),
+                    ),
+                  ),
+                ),
+                const Positioned(
+                    top: 0,
+                    right: 0,
+                    child: TCircularIcon(
+                      icon: Iconsax.heart5,
+                      color: Colors.red,
+                    ))
+              ],
+            ),
+          ),
+          SizedBox(
+            width: 172,
+            child: Padding(
+              padding: const EdgeInsets.only(top: TSizes.sm, left: TSizes.sm),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TProductTitleText(
+                        title: 'Green Nike Shoe',
+                        smallSize: false,
+                      ),
+                      SizedBox(
+                        height: TSizes.spaceBtwItems / 2,
+                      ),
+                      TBrandTitleWithVerificationIcon(title: 'Nike'),
+                    ],
+                  ),
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Flexible(child: TProductPriceText(price: '250.0')),
+                      Container(
+                        decoration: const BoxDecoration(
+                            color: TColors.black,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(TSizes.cardRadiusMd),
+                              bottomRight:
+                                  Radius.circular(TSizes.productImageRadius),
+                            )),
+                        child: const SizedBox(
+                            height: TSizes.iconLg * 1.2,
+                            width: TSizes.iconLg * 1.2,
+                            child: Center(
+                                child: Icon(
+                              Iconsax.add,
+                              color: TColors.white,
+                            ))),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
