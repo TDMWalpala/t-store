8af7035a874a7c79597bd37202a9c1281b7f52b6
TDMWalpala <tharindudarshana2019@gmail.com>
2024-04-12 21:03:45 +0530

    TA-23:Completed Order Page

---
+++ lib/features/shop/screens/order/widgets/orders_list.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+
+class TOrderListItems extends StatelessWidget {
+  const TOrderListItems({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return ListView.separated(
+      itemCount: 10,
+      shrinkWrap: true,
+      separatorBuilder: (_, index) => const SizedBox(height: TSizes.spaceBtwItems,),
+      itemBuilder: (_, index) => TCircularContainer(
+        showBorder: true,
+        background: dark ? TColors.dark : TColors.light,
+        padding: const EdgeInsets.all(TSizes.md),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                Icon(Iconsax.ship),
+                const SizedBox(
+                  width: TSizes.defaultSpace / 2,
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Processing',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge!
+                            .apply(color: TColors.primary, fontWeightDelta: 1),
+                      ),
+                      Text(
+                        '04 Apr 2024',
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                    ],
+                  ),
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Iconsax.arrow_right_34,
+                      size: TSizes.iconSm,
+                    )),
+              ],
+            ),
+            const SizedBox(height: TSizes.spaceBtwItems,),
+            Row(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Icon(Iconsax.tag),
+                      const SizedBox(
+                        width: TSizes.defaultSpace / 2,
+                      ),
+                      Expanded(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Order',
+                              style: Theme.of(context).textTheme.labelMedium,
+                            ),
+                            Text(
+                              '[#256f7]',
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Icon(Iconsax.calendar),
+                      const SizedBox(
+                        width: TSizes.defaultSpace / 2,
+                      ),
+                      Expanded(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Shipping Date',
+                              style: Theme.of(context).textTheme.labelMedium,
+                            ),
+                            Text(
+                              '06 May 2024',
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
