9b5718a3f39c2fa65f4c3b19ecd802bdc3c09f34
TDMWalpala <tharindudarshana2019@gmail.com>
2024-04-13 14:26:16 +0530

    TA-26:Completed Firebase setup & LocalStorage

--- lib/features/auth/controllers.onboarding/onboarding.controller.dart
+++ lib/features/auth/controllers.onboarding/onboarding.controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:t_store/features/auth/screens/login/login.dart';
 
 class OnBoardingController extends GetxController {
@@ -18,7 +19,11 @@ class OnBoardingController extends GetxController {
 
   void nextPages() {
     if (currentPageIndex.value == 2) {
+      
+      final storage = GetStorage();
+      storage.write('isFirstTime', false);
       Get.offAll(const LoginScreen());
+
     } else {
       int page = currentPageIndex.value + 1;
       pageController.jumpToPage(page);
